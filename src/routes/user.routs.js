245b74f8548273e70caac106dbfd595c340490b3
prrraveenkumar <pk248539@gmail.com>
2025-11-21 00:57:59 +0530

    feat: add additional user routes for password change, current user retrieval, account updates, and user media uploads

--- src/routes/user.routs.js
+++ src/routes/user.routs.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser, refreshAccessToken } from "../controller/user.controller.js";
+import { registerUser,loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controller/user.controller.js";
 import {upload} from "../middleware/multer.middelware.js"
 import { verifyJWT } from "../middleware/auth.middelware.js";
+import multer from "multer";
 
 const router = Router()
 
@@ -24,6 +25,20 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT , logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken )
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword )
+
+router.route("/current-user").get(verifyJWT, getCurrentUser )
+
+router.route("/updated-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar )
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/cover-image"), updateUserCoverImage )
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("history").post(verifyJWT, getWatchHistory)
    
 
 
